2b62366727377d0fb66bdf2edf37da5f36addc14
Jonas Foghsgaard <jonas.foghsgaard@gmail.com>
2018-10-30 11:46:50 +0100

    Jeg har tilføjet metoder i klassen Terninger og set de virker i Terningspilklassen

--- src/Terninger.java
+++ src/Terninger.java
@@ -1,2 +1,12 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Terninger {
+    public static int getSum(int n1, int n2){ // metoden til at få summen af terningerne.
+        int sum = n1+n2;
+        return sum;
+    }
+    public static int roll() {
+        int T2 = ThreadLocalRandom.current().nextInt(1, 6 + 1);  // genererer et tilfældigt tal mellem 1 og 6
+        return T2;
+    }
 }
--- src/Terningspil.java
+++ src/Terningspil.java
@@ -1,2 +1,11 @@
 public class Terningspil {
+    public static void main(String[] args) {
+        int a = Terninger.roll();
+        int b = Terninger.roll();
+        int c = Terninger.getSum(a,b);
+        System.out.println(a + " " +b + " " + c);
+
+
+    }
 }
+
